c6f2bc61f80bfc1928d6c09cc05216d76d946bee
CaptainSuf <727473069@qq.com>
2018-11-13 20:25:09 +0800

    201811132024-sufeng-初始化

---
+++ src/main/java/com/dev/ctrl/LoginCtrl.java
@@ -0,0 +1,20 @@
+package com.dev.ctrl;
+
+import com.dev.model.TbUserLoginPO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginCtrl {
+
+    @ResponseBody
+    @RequestMapping(value = "/login.do")
+    public String login(TbUserLoginPO user){
+        if("admin".equals(user.getUserName()) && "123456".equals(user.getPassword())){
+            return "登录成功";
+        }else {
+            return "登录失败";
+        }
+    }
+}
---
+++ src/main/java/com/dev/model/TbUserLoginPO.java
@@ -0,0 +1,23 @@
+package com.dev.model;
+
+public class TbUserLoginPO {
+
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
