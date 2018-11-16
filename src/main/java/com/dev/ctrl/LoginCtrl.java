616311b6cc82765a620b45249baa7d6f2a642bf2
CaptainSuf <727473069@qq.com>
2018-11-16 16:07:27 +0800

    201811161607-sufeng-兼容feign接口

--- src/main/java/com/dev/ctrl/LoginCtrl.java
+++ src/main/java/com/dev/ctrl/LoginCtrl.java
@@ -2,6 +2,7 @@ package com.dev.ctrl;
 
 import com.dev.model.TbUserLoginPO;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,7 +11,7 @@ public class LoginCtrl {
 
     @ResponseBody
     @RequestMapping(value = "/login.do")
-    public String login(TbUserLoginPO user){
+    public String login(@RequestBody TbUserLoginPO user){
         if("admin".equals(user.getUserName()) && "123456".equals(user.getPassword())){
             return "登录成功";
         }else {
